9ac5d413f695e1c78fd844a695366b548ec5d385
Nitesh <jobkomail@gmail.com>
2022-01-06 22:33:05 +1100

    Vets Index page and Vets controller setup completed, Closes #7

---
+++ pet-clinic-web/src/main/java/com/niteshb/nitpetclinic/controllers/VetsController.java
@@ -0,0 +1,15 @@
+package com.niteshb.nitpetclinic.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetsController {
+
+    @RequestMapping({"/vets", "/vets/index"})
+    public String listVets(){
+
+        return "vets/index";
+    }
+}
