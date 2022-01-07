33f5c4b8ed22b337cc7791ccdccc7411e5682dc3
Nitesh <jobkomail@gmail.com>
2022-01-07 14:09:48 +1100

    All vets are listed; completed, Closes #10

--- pet-clinic-web/src/main/java/com/niteshb/nitpetclinic/controllers/VetsController.java
+++ pet-clinic-web/src/main/java/com/niteshb/nitpetclinic/controllers/VetsController.java
@@ -1,15 +1,25 @@
 package com.niteshb.nitpetclinic.controllers;
 
 
+import com.niteshb.nitpetclinic.services.VetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetsController {
 
-    @RequestMapping({"/vets", "/vets/index"})
-    public String listVets(){
+    private final VetService vetService;
 
+    @Autowired
+    public VetsController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"/vets", "/vets/index"})
+    public String listVets(Model model){
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
