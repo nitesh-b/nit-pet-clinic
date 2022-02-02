60320258217730be4b10508e1d96ead37a79c451
Nitesh <nitesh@talk5.audio>
2022-02-03 10:18:44 +1100

    Updated base Entity.  Closes #30

--- pet-clinic-data/src/main/java/com/niteshb/nitpetclinic/models/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/niteshb/nitpetclinic/models/BaseEntity.java
@@ -1,8 +1,15 @@
 package com.niteshb.nitpetclinic.models;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; //hibernate recommendation: primitive could be null, not this one
 
     public Long getId() {
