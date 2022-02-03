4ce9e21907dff7975293b1c08dd0bd4674e30faf
Nitesh <nitesh@talk5.audio>
2022-02-03 17:09:01 +1100

    Created spring JPA repositories.  Closes #34

--- pet-clinic-data/src/main/java/com/niteshb/nitpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/niteshb/nitpetclinic/repositories/OwnerRepository.java
@@ -4,4 +4,5 @@ import com.niteshb.nitpetclinic.models.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+    Owner findByLastName(String lastname);
 }
